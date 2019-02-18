5a7dc26e552aca7a59f26422395098ae8978596e
Michael A. Vickers <mavickers@gmail.com>
2019-02-18 13:42:51 -0500

    More reference fixing.

--- src/vue-toolkit.js
+++ src/vue-toolkit.js
@@ -2,6 +2,10 @@ import vuex from "./vuex/vuex";
 
 export const mapStateLazily = vuex.mappings.mapStateLazily;
 
+export const buildMutations = vuex.mutations.buildMutations;
+export const noMutator = vuex.mutations.noMutator;
+export const straightMutator = vuex.mutations.straightMutator;
+
 export default {
     vuex: vuex
 }
