76b27264dc95700f34854b8ca19a0b74474dab9a
Michael A. Vickers <mavickers@gmail.com>
2019-02-19 14:25:46 -0500

    Reorg project folder; move dist folder outside of project.

--- src/watch.js
+++ src/watch.js
@@ -2,6 +2,9 @@ const del = require("del");
 const copy = require("recursive-copy");
 const jsonFile = require("jsonfile");
 
+// change this to your liking
+const distFolder = "../../../dist";
+
 const copyOptions = {
     filter: [ /^((?!(node_modules|\.idea|package-lock\.json|watch\.js)).)*$/ ]
 };
@@ -9,17 +12,17 @@ const copyOptions = {
 async function main() {
 
     try{
-        await del.sync(["../dist"], { force: true });
+        await del.sync([distFolder], { force: true });
 
-        await copy(".", "../dist", copyOptions);
+        await copy(".", distFolder, copyOptions);
 
-        const obj = await jsonFile.readFile("../dist/package.json");
+        const obj = await jsonFile.readFile(distFolder + "/package.json");
 
         delete obj.devDependencies;
         delete obj.scripts.dev;
         delete obj.scripts.watchbuild;
 
-        await jsonFile.writeFile("../dist/package.json", obj, { spaces: 4, EOL: "\r\n" });
+        await jsonFile.writeFile(distFolder + "/package.json", obj, { spaces: 4, EOL: "\r\n" });
 
         console.log("build complete\r\n");
     }
